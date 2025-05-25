f9f098042871e6dc153b542ebfcd3e0bb286a3a8
Andrew <andrew.e.tacoi@gmail.com>
2025-05-25 00:02:16 -0400

    Basic layout done

---
+++ vite-direct/src/components/Home.jsx
@@ -0,0 +1,24 @@
+import "../css/Home.css";
+
+function Home() {
+  return (
+    <div id="home-container">
+      <h1>Welcome!</h1>
+      <p>
+        Behold, the best and most wonderfully put together site you will ever
+        lay your eyes upon!
+      </p>
+      <p>A place for creativity and innovation to shine.</p>
+      <p>
+        I present to you: the personal website of yours truly, Andrew! (whoose
+        last name will not be mentioned)
+      </p>
+      <p>
+        Please, feel free to visit the many subpages using the navigation bar
+        above!
+      </p>
+    </div>
+  );
+}
+
+export default Home;
