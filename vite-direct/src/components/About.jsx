3bf4f258c9a5a926bc98d42f66630551e1187625
Andrew <andrew.e.tacoi@gmail.com>
2025-08-02 02:14:55 -0400

    socials added

--- vite-direct/src/components/About.jsx
+++ vite-direct/src/components/About.jsx
@@ -1,3 +1,27 @@
-function About() {}
+import "../css/About.css";
+
+function About() {
+  return (
+    <div id="about-container">
+      <h1>Socials</h1>
+      <ul>
+        <li id="slide">
+          <a className="page" href="https://github.com/atacoi" target="_blank">
+            GitHub
+          </a>
+        </li>
+        <li id="slide">
+          <a
+            className="page"
+            href="https://leetcode.com/u/atacoi/"
+            target="_blank"
+          >
+            LeetCode
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
 
 export default About;
