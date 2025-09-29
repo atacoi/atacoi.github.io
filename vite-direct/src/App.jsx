c22b9756de1902a0f89aa126be04537e30729470
Andrew <andrew.e.tacoi@gmail.com>
2025-09-29 01:24:33 -0400

    updated overall design

--- vite-direct/src/App.jsx
+++ vite-direct/src/App.jsx
@@ -4,12 +4,38 @@ import GitHub from "./assets/github.png";
 import Leetcode from "./assets/leetcode.png";
 import LinkedIn from "./assets/linkedin.png";
 import walk from "./assets/walk.gif";
+import number from "./assets/numbers.gif";
+import pin from "./assets/pin.webp";
 
 function App() {
   return (
-    <div id="App">
-      <div className="box" style={{ margin: "10% 0 5px 0" }}>
-        <h1>Andrew Tacoi</h1>
+    <div id="App" style={{ marginTop: "27px" }}>
+      <img
+        src={pin}
+        style={{
+          width: "45px",
+          height: "auto",
+          position: "absolute",
+          top: "6px",
+          zIndex: "2",
+        }}
+      ></img>
+      <div
+        style={{
+          border: "2px solid black",
+          width: "45%",
+          maxWidth: "500px",
+          height: "30px",
+          padding: 0,
+          borderTop: "2px solid black",
+          borderBottom: "none",
+          zIndex: "1",
+          backgroundColor: "transparent",
+        }}
+      ></div>
+
+      <div className="box" style={{ margin: "0 0 0 0" }}>
+        <h1 style={{ margin: "10px 0 5px 0" }}>Andrew Tacoi</h1>
         <div id="socials">
           <a href="https://github.com/atacoi" target="_blank">
             <img src={GitHub}></img>
@@ -26,11 +52,35 @@ function App() {
         </div>
       </div>
 
-      <div className="box" style={{ marginBottom: "15%" }}>
+      <div
+        style={{
+          border: "2px solid black",
+          width: "45%",
+          height: "10px",
+          padding: 0,
+          borderBottom: "none",
+          borderTop: "none",
+          maxWidth: "500px",
+        }}
+      ></div>
+
+      <div className="box">
         <div>Welcome to my professonal website!</div>
       </div>
 
-      <div className="box" style={{ marginBottom: "15%" }}>
+      <div
+        style={{
+          border: "2px solid black",
+          width: "45%",
+          height: "150px",
+          padding: 0,
+          borderBottom: "none",
+          borderTop: "none",
+          maxWidth: "500px",
+        }}
+      ></div>
+
+      <div className="box" style={{ marginBottom: "0" }}>
         <h2 style={{ margin: "10px 0" }}>Bio</h2>
 
         <div>
@@ -52,11 +102,19 @@ function App() {
           an undergraduate student.
         </div>
       </div>
-
       <div
-        className="box"
-        style={{ padding: "1em 0 0 0", marginBottom: "2em" }}
-      >
+        style={{
+          border: "2px solid black",
+          width: "45%",
+          height: "150px",
+          padding: 0,
+          borderBottom: "none",
+          borderTop: "none",
+          maxWidth: "500px",
+        }}
+      ></div>
+
+      <div className="box" style={{ paddingBottom: 0, marginBottom: "2em" }}>
         <h2 style={{ margin: "10px 0" }}>Projects</h2>
         <div id="Projects">
           <Project
@@ -71,10 +129,19 @@ function App() {
             href={"http://google.com"}
             des="test des"
           />
+        </div>
+
+        <div id="Projects">
           <Project
             name="Simple Ray Caster"
             imgSrc={walk}
-            href={"http://google.com"}
+            href={"https://github.com/atacoi/Simple-Raycaster"}
+            des="test des"
+          />
+          <Project
+            name="Numerical System Converter"
+            imgSrc={number}
+            href={"https://github.com/atacoi/Number-System-Converter"}
             des="test des"
           />
         </div>
